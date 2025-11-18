077fa81dbd2b9968475c14859bd00ef76d79399f
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-11-18 16:35:08 +0900

    Create arduino_uno_r4_broker_connet_and_publish.ino

---
+++ Arduino_UNO_R4_WiFi/arduino_uno_r4_broker_connet_and_publish.ino
@@ -0,0 +1,56 @@
+#include <WiFiS3.h>
+#include <ArduinoMqttClient.h>
+int status = WL_IDLE_STATUS;
+
+// --------------------- WiFi 정보 -------------------------
+char ssid[] = "Ria2G";
+char password[] = "730124go";
+
+// --------------------- MQTT 브로커 정보 -------------------
+const char broker[] = "192.168.211.47";   // MQTT 서버 주소
+int        port     = 1883;
+
+// --------------------- MQTT 인증 정보 ---------------------
+const char mqttUser[] = "ship";     // MQTT ID
+const char mqttPass[] = "1234"; // MQTT Password
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  // 공유기 연결 시도
+  Serial.print("Connecting to " + String(ssid));
+  while (status != WL_CONNECTED) {
+    Serial.print(".");
+    status = WiFi.begin(ssid, password);
+    delay(1000);  // 재시도 간격
+  }
+  Serial.println("\nWiFi 연결 성공!");
+  Serial.println("Arduino Board의 IP 주소: " + WiFi.localIP().toString());
+
+  // ---------- MQTT 설정 ----------
+  mqttClient.setUsernamePassword(mqttUser, mqttPass);
+
+  Serial.print("MQTT 브로커 연결중...");
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("실패! 오류 코드: ");
+    Serial.println(mqttClient.connectError());
+    delay(1000);
+  }
+  Serial.println("성공!");
+  mqttClient.setKeepAliveInterval(60); // 옵션
+}
+
+void loop() {
+  mqttClient.poll(); // 연결 유지 필수
+
+  Serial.println("Publish 메세지 전송!");
+  mqttClient.beginMessage("test/topic");
+  mqttClient.print("Hello from UNO R4 WiFi + MQTT Auth");
+  mqttClient.endMessage();
+
+  delay(2000);
+}
